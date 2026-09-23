96883b09b398a4ea45b0af18f854a2f57f9636ac
Guilherme Santos <110872956+GuiZeroUm@users.noreply.github.com>
2026-09-23 19:17:37 +0000

    fix(checkout): allow CNPJ in boleto payment

---
+++ frontend/src/utils/formatTaxId.js
@@ -0,0 +1,18 @@
+export const formatTaxId = value => {
+  const digits = String(value || "")
+    .replace(/\D/g, "")
+    .slice(0, 14);
+
+  if (digits.length <= 11) {
+    return digits
+      .replace(/^(\d{3})(\d)/, "$1.$2")
+      .replace(/^(\d{3})\.(\d{3})(\d)/, "$1.$2.$3")
+      .replace(/\.(\d{3})(\d)/, ".$1-$2");
+  }
+
+  return digits
+    .replace(/^(\d{2})(\d)/, "$1.$2")
+    .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+    .replace(/\.(\d{3})(\d)/, ".$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2");
+};
